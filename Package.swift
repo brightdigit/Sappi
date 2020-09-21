1cb735b17c97be8fe351370e1d453b2d3c68f58e
Leo Dion <leogdion@brightdigit.com>
2020-09-21 17:09:29 -0400

    adding sysinfo and interface

--- Package.swift
+++ Package.swift
@@ -5,17 +5,23 @@ import PackageDescription
 
 let package = Package(
     name: "sappi",
+    platforms: [.macOS(.v10_15)],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
       .package(url: "https://github.com/apple/swift-argument-parser.git", from: "0.3.0"),
+      .package(url: "https://github.com/alqemyst/perfect-sysinfo.git", .branch("master")),
+      .package(name: "NetUtils", url: "https://github.com/ivirtex/swift-netutils.git", from: "4.2.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "sappi", dependencies: [
+              .product(name: "PerfectSysInfo", package: "perfect-sysinfo"),
               .product(name: "ArgumentParser", package: "swift-argument-parser"),
+              "NetUtils"
+              
           ]),
         .testTarget(
             name: "sappiTests",
--- Sources/sappi/main.swift
+++ Sources/sappi/main.swift
@@ -1,3 +1,26 @@
 import ArgumentParser
+import PerfectSysInfo
+import NetUtils
 
-print("Hello, world!")
+/**
+ System load:            0.0
+ Usage of /:             5.5% of 116.92GB
+ Memory usage:           28%
+ Swap usage:             0%
+ Temperature:            49.9 C
+ Processes:              149
+ Users logged in:        1
+ IPv4 address for eth0:  192.168.1.109
+ IPv6 address for eth0:  2600:1702:4050:7d30::505
+ IPv6 address for eth0:  2600:1702:4050:7d30:ba27:ebff:fef4:1c1e
+ IPv4 address for wlan0: 192.168.1.120
+ IPv6 address for wlan0: 2600:1702:4050:7d30::3f1
+ IPv6 address for wlan0: 2600:1702:4050:7d30:ba27:ebff:fea1:494b
+ */
+
+print(SysInfo.CPU)
+print(SysInfo.Memory)
+print(SysInfo.Disk)
+print(SysInfo.Net)
+print(Interface.allInterfaces())
+
