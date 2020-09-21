7f959b8b7ee26ec661bd0dacfd3170e671bc0f13
Leo Dion <leogdion@brightdigit.com>
2020-09-21 16:22:08 -0400

    added argument parser

--- Package.swift
+++ Package.swift
@@ -8,13 +8,15 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+      .package(url: "https://github.com/apple/swift-argument-parser.git", from: "0.3.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "sappi",
-            dependencies: []),
+            name: "sappi", dependencies: [
+              .product(name: "ArgumentParser", package: "swift-argument-parser"),
+          ]),
         .testTarget(
             name: "sappiTests",
             dependencies: ["sappi"]),
