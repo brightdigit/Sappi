067858310f127f3ede8c1e026fa77409668d7cab
Leo Dion <leogdion@brightdigit.com>
2020-09-22 09:34:44 -0400

    calcluating cpu usage

--- Sources/sappi/main.swift
+++ Sources/sappi/main.swift
@@ -1,7 +1,7 @@
 import ArgumentParser
 import PerfectSysInfo
 import NetUtils
-
+import IOKit
 /**
  System load:            0.0
  Usage of /:             5.5% of 116.92GB
@@ -18,9 +18,21 @@ import NetUtils
  IPv6 address for wlan0: 2600:1702:4050:7d30:ba27:ebff:fea1:494b
  */
 
-print(SysInfo.CPU)
-print(SysInfo.Memory)
-print(SysInfo.Disk)
-print(SysInfo.Net)
-print(Interface.allInterfaces())
+//print(SysInfo.CPU)
+
+let cpuValue : Double?
+if let cpu = SysInfo.CPU["cpu"] {
+  let cpuSum = cpu.map{$0.value}.reduce(0, +)
+  let cpuIdle = cpu["idle"] ?? 0
+  cpuValue = 1.0 - Double(cpuIdle)/Double(cpuSum)
+} else {
+  cpuValue = nil
+}
+
+print(cpuValue)
+//print(SysInfo.Memory)
+//print(SysInfo.Disk)
+//print(SysInfo.Net)
+//print(Interface.allInterfaces())
+
 
