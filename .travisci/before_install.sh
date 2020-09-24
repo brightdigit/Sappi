145b5a96dad76904da0db9459bc94a1cf078b495
leogdion <leogdion@brightdigit.com>
2020-09-24 15:45:22 -0400

    fixing arm installation

--- .travisci/before_install.sh
+++ .travisci/before_install.sh
@@ -9,7 +9,7 @@ elif [[ $TRAVIS_OS_NAME = 'linux' ]]; then
   
   if [[ $RELEASE_DOT == "20.04" ]]; then
     sudo apt-get update
-    sudo apt-get -y install \
+    sudo apt-get -y -f install \
             binutils \
             git \
             gnupg2 \
@@ -29,7 +29,7 @@ elif [[ $TRAVIS_OS_NAME = 'linux' ]]; then
     
   if [[ $TRAVIS_CPU_ARCH == "arm64" ]]; then
     curl -s https://packagecloud.io/install/repositories/swift-arm/release/script.deb.sh | sudo bash
-    sudo apt-get install swift5
+    sudo apt-get -y -f install swiftlang
   else 
     wget https://swift.org/builds/swift-${SWIFT_VER}-release/ubuntu${RELEASE_NUM}/swift-${SWIFT_VER}-RELEASE/swift-${SWIFT_VER}-RELEASE-ubuntu${RELEASE_DOT}.tar.gz
     tar xzf swift-${SWIFT_VER}-RELEASE-ubuntu${RELEASE_DOT}.tar.gz
